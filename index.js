05d21bf4a5d623a35a3eff8d10854d3f365bc5c5
stefandoerger <doerger.stefan@gmail.com>
2021-12-07 16:59:56 +0100

    Add BaseState, #main

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ import { defineStore } from "pinia";
 // LIBS
 import justSafeGet from "just-safe-get";
 
+export class BaseState {
+  //Indicates if the request has finished
+  isFinished = undefined;
+  //Indicates if the request is currently loading
+  isLoading = undefined;
+  //Indicates if the request was canceled
+  error = undefined;
+  // //Axios response data
+  response = undefined;
+
+  constructor(
+    isFinished = false,
+    isLoading = false,
+    error = false,
+    response = undefined
+  ) {
+    this.isFinished = isFinished || false;
+    this.isLoading = isLoading || false;
+    this.error = error || false;
+    this.response = response || undefined;
+  }
+}
+
 export class Retonio {
   storeId = undefined; // Pinia ID / AlertMapper
   apiCall = undefined; // Imported API call
