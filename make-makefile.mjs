bbf4498d33196f7533cdf7b3852a5cec07309dec
stefandoerger <doerger.stefan@gmail.com>
2021-12-13 22:26:13 +0100

    MV maker, #main

---
+++ make-makefile.mjs
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+function createContent() {
+  return `retonio:
+	node winlocal/script/make-retonio.mjs js false	$(filter-out $@,$(MAKECMDGOALS))`;
+}
+
+function createFile(path) {
+  // ONLY if file does not exisit
+  fs.writeFile("Makefile", createContent(), { flag: "wx" }, function (err) {
+    if (err) {
+      console.log("Error");
+      return console.log(`Makefile already exists!`);
+    }
+    return console.log(`Makefile was created.`);
+  });
+}
+
+createFile();
