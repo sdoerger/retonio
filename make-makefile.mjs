7bf901ac28833c46fd213f61314bb35b56124a53
stefandoerger <doerger.stefan@gmail.com>
2021-12-13 22:37:28 +0100

    Fix makefile path, #main

--- make-makefile.mjs
+++ make-makefile.mjs
@@ -2,7 +2,7 @@ import fs from "fs";
 
 function createContent() {
   return `retonio:
-	node winlocal/script/make-retonio.mjs js false	$(filter-out $@,$(MAKECMDGOALS))`;
+	node node_modules/retonio/make-retonio.mjs js false	$(filter-out $@,$(MAKECMDGOALS))`;
 }
 
 function createFile(path) {
